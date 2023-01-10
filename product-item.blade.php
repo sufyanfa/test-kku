fbc4f570dd7bb3b766d85adeee34ca916d01d4d0
Sufyan Farea <33679576+sufyanfa@users.noreply.github.com>
2023-01-10 14:57:18 +0300

    Create product-item.blade.php

---
+++ product-item.blade.php
@@ -0,0 +1,40 @@
+<section class="bg-gray-100 rounded-b-10xl overflow-hidden pt-12 pb-24">
+  <div class="container mx-auto px-4">
+    <div class="-mx-4 flex flex-wrap">
+      <div class="w-full px-4 lg:w-1/2">
+        <div class="mb-6 max-w-md">
+          <span class="text-xs tracking-wider text-gray-400">
+            APPLE #3299803</span>
+          <h2 class="font-heading mt-6 mb-4 text-5xl font-medium md:text-7xl lg:text-8xl">Apple iPhone 12 Pro (128GB) - Silver</h2>
+          <p class="mb-6 flex items-center">
+            <span class="mr-2 text-sm font-medium text-thir">$</span>
+            <span class="text-3xl font-medium text-thir">44.90</span>
+          </p>
+          <p class="text-lg text-gray-400">The nulla commodo, commodo eros a lor, tristique lectus. Lorem sad 128 GB silver.</p>
+        </div>
+       
+     
+        <div class="mb-10">
+          <h4 class="font-heading mb-3 font-medium">Qty:</h4>
+          <input class="w-24 rounded-xl border-2 border-blue-500 bg-white px-3 py-2 text-center outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50" type="text" placeholder="1" />
+        </div>
+        <div class="-mx-2 mb-12 flex flex-wrap">
+          <div class="mb-2 w-full px-2 md:mb-0 md:w-2/3"><a class="font-heading block rounded-xl bg-blue-500 py-4 px-2 text-center text-xl font-medium leading-8 tracking-tighter text-white hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50" href="#">Add to bag</a></div>
+          <div class="w-full px-2 md:w-1/3">
+          
+          </div>
+        </div>
+      </div>
+      <div class="w-full lg:w-1/2 px-4 mb-16 lg:mb-0">
+        <div class="flex -mx-4 flex-wrap items-center justify-between lg:justify-start lg:items-start xl:items-center">
+          <div class="w-full sm:w-auto min-w-max px-4 text-center flex sm:flex-col items-center justify-center">
+            
+            <a class="h-30 block mb-4 mr-2 sm:mr-0" href="#">
+              <img class="h-full w-full" src="https://images.unsplash.com/photo-1661956601349-f61c959a8fd4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80" alt="">
+            </a>
+            
+          </div>
+        </div>
+    </div>
+  </div>
+</section>
