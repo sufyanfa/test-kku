c9b48dd5d861e7644c34b8fead8875313138fb58
Sufyan Farea <33679576+sufyanfa@users.noreply.github.com>
2023-01-10 10:08:59 +0300

    Create products.blade.php

---
+++ products.blade.php
@@ -0,0 +1,22 @@
+<a class="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition" href="#">
+      <div class="p-4 md:p-5">
+        <div class="flex justify-between items-center">
+          <div class="flex items-center">
+            <img class="h-[2.375rem] w-[2.375rem] rounded-full" src="https://images.unsplash.com/photo-1597740049284-388659a41286?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Image Description">
+            <div class="mr-3">
+             <h3 class="group-hover:text-thir font-semibold text-gray-800">
+              {{store->title}}
+            </h3>
+            <p class="text-sm text-gray-500">
+              26 منتج
+            </p>
+            </div>
+          </div>
+          <div class="pr-3">
+            <svg class="w-4 h-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </a>
